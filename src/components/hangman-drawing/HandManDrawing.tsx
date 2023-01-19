9b10f7dea8ebdbf79ff050b9173de48d98e1b57f
walissonsou <walissonsouza96@hotmail.com>
2023-01-19 13:45:16 -0300

    feat: style forca

--- src/components/hangman-drawing/HandManDrawing.tsx
+++ src/components/hangman-drawing/HandManDrawing.tsx
@@ -1,5 +1,28 @@
+import styled from 'styled-components'
+
+const Base = styled.div`
+height: 10px;
+width: 250px;
+background-color: #693333;;
+`
+const Pau = styled.div`
+height: 250px;
+width: 10px;
+background-color: #693333;;
+`
+const Corda = styled.div`
+margin-left: 115px;
+height: 10px;
+width: 125px;
+background-color: #693333;
+position: absolute;
+`
 export const HandManDrawing = () => {
   return (
-    <div> HandManDrawing </div>
+    <div style={{ position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center'}}> 
+    <Corda/>
+    <Pau />
+    <Base />
+    </div>
   )
 }
