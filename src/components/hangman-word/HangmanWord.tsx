acad3be9e116ffac06f68685d82a4e948239d1b6
walissonsou <walissonsouza96@hotmail.com>
2023-01-19 15:12:29 -0300

    feat: logica HadmanWord

--- src/components/hangman-word/HangmanWord.tsx
+++ src/components/hangman-word/HangmanWord.tsx
@@ -1,6 +1,23 @@
+import styled from 'styled-components'
 
+const Wrapper = styled.div`
+display: flex;
+justify-content: center;
+gap: 0.5rem;
+font-size: 4rem;
+font-weight: bold;
+text-transform: uppercase;
+margin-top: 1rem;
+`
 export const HangmanWord = () => {
+  const palavra = "t e s t e"
+  const adivinhaLetra = ['t', 'e', 'h']
   return (
-    <div> HangmanWord </div>
+    <Wrapper> {palavra.split("").map((letras, index) => (
+      <span style={{borderBottom: '0.1em solid black', height: '35px'}} key={index}> 
+      <span style={{visibility: adivinhaLetra.includes(letras) ? 'visible' : 'hidden'}}> { letras }</span>    
+</span>
+      
+    ))} </Wrapper>
   )
 }
