2d71ee3d0188dcff48e7eb332912227eaa6edceb
walissonsou <walissonsouza96@hotmail.com>
2023-01-25 23:47:51 -0300

    feat:  letras adivinhadas vindo do useState

--- src/components/hangman-word/HangmanWord.tsx
+++ src/components/hangman-word/HangmanWord.tsx
@@ -10,34 +10,24 @@ font-weight: bold;
 text-transform: uppercase;
 margin-top: 1rem;
 `
-interface HandManWordsProps {
+interface wordProps {
   word: string;
+  letrasAdivinhadas: string[];
 }
-export const HangmanWord = ({word} : HandManWordsProps) => {
+export const HangmanWord = ({word, letrasAdivinhadas} : wordProps ) => {  
   
-  const adivinhaLetra = ['']  
-  
- 
 
   return (
     <div>
       <>
         <Wrapper> {word.split("").map((letras, index) => (
           <span style={{ borderBottom: '0.1em solid black', height: '35px' }} key={index}>
-            <span style={{ visibility: adivinhaLetra.includes(letras) ? 'visible' : 'hidden' }}> {letras}
+            <span style={{ visibility: letrasAdivinhadas.includes(letras) ? 'visible' : 'hidden' }}> {letras}
             </span>
           </span>
 
         ))} </Wrapper>
       </>
-      {/* <form>
-        <label>     
-          Letras:     
-          <input type="text" onChange={(e) => setLetras(e.target.value)}/>
-          <button disabled={letras.length > 1}> Enviar </button>
-        </label>          
-      </form> */}
-
     </div>
   )
 }
